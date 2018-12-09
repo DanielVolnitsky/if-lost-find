b71886a7ab7eb94d88bfa6a924e19685463e8bee
Daniel Volnitsky <waytodanny@gmail.com>
2018-12-09 20:14:58 +0200

    init configuration

---
+++ project/src/main/java/com/daniel/iflostfind/IfLostFindApplication.java
@@ -0,0 +1,12 @@
+package com.daniel.iflostfind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfLostFindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IfLostFindApplication.class, args);
+    }
+}
---
+++ project/src/test/java/com/daniel/iflostfind/IfLostFindApplicationTests.java
@@ -0,0 +1,16 @@
+package com.daniel.iflostfind;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IfLostFindApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
