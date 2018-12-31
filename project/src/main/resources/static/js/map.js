d1b33d1db1bfa6c2cd672e0bf56f6aed57d64576
psidae <akson.rill@gmail.com>
2018-12-31 14:59:26 +0200

    Add Map

--- project/src/main/resources/static/js/map.js
+++ project/src/main/resources/static/js/map.js
@@ -1,19 +1,47 @@
-// Initialize and add the map
 function initMap() {
-    // The location of Uluru
-    let uluru = {
-        lat: -25.344,
-        lng: 131.036
+
+    let planetarium = {
+        lat: 50.431782,
+        lng: 30.516382
     };
-    // The map, centered at Uluru
+
     let map = new google.maps.Map(
         document.getElementById('map'), {
-            zoom: 4,
-            center: uluru
+            zoom: 20,
+            center: planetarium
         });
-    // The marker, positioned at Uluru
-    let marker = new google.maps.Marker({
-        position: uluru,
-        map: map
+
+    let contentString = '<div id="content">' +
+        '<div id="siteNotice">' +
+        '</div>' +
+        '<h1 id="firstHeading" class="firstHeading">Request №12</h1>' +
+        '<div id="bodyContent">' +
+        '<p><b>Lost Phone</b></p>' +
+        '<p>Lost phone near planetarium </p>' +
+        '<p>Author: test@test.com</p>' +
+        '<p>Attr: Black Phone, without screen</p>' +
+        '<p>(Posted 29.02.2018).</p>' +
+        '</div>' +
+        '</div>';
+
+    let infowindow = new google.maps.InfoWindow({
+        content: contentString
     });
+
+
+    let marker = new google.maps.Marker({
+        position: planetarium,
+        map: map,
+        title: "Planetarium",
+        icon: '../images/dolma.png',
+    });
+
+    marker.addListener('click', function () {
+        infowindow.open(map, marker);
+    });
+
 }
+
+$('.ui.sidebar').sidebar({
+    context: $('.bottom.segment')
+}).sidebar('attach events', '.menu .item');
