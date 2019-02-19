aaba7d10453064893c8cffbf488f52c7dee6a4bf
psidae <akson.rill@gmail.com>
2019-02-19 13:07:32 +0100

    Add fullpage js boxes, update view, add flex-type

--- project/src/main/resources/static/js/map.js
+++ project/src/main/resources/static/js/map.js
@@ -1,47 +1,103 @@
+const labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+let map, infowindow, placeSearch;
+let labelIndex = 0;
+
 function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 50.431782,
+            lng: 30.516382
+        },
+        zoom: 20
+    });
 
-    let planetarium = {
-        lat: 50.431782,
-        lng: 30.516382
-    };
+    infoWindow = new google.maps.InfoWindow;
 
-    let map = new google.maps.Map(
-        document.getElementById('map'), {
-            zoom: 20,
-            center: planetarium
+    autocomplete = new google.maps.places.Autocomplete(
+        document.getElementById('autocomplete'), {
+            types: ['geocode']
         });
 
-    let contentString = '<div id="content">' +
-        '<div id="siteNotice">' +
-        '</div>' +
-        '<h1 id="firstHeading" class="firstHeading">Request №12</h1>' +
-        '<div id="bodyContent">' +
-        '<p><b>Lost Phone</b></p>' +
-        '<p>Lost phone near planetarium </p>' +
-        '<p>Author: test@test.com</p>' +
-        '<p>Attr: Black Phone, without screen</p>' +
-        '<p>(Posted 29.02.2018).</p>' +
-        '</div>' +
-        '</div>';
-
-    let infowindow = new google.maps.InfoWindow({
-        content: contentString
+    // This event listener calls addMarker() when the map is clicked.
+    google.maps.event.addListener(map, 'click', function (event) {
+        addMarker(event.latLng, map);
     });
 
 
-    let marker = new google.maps.Marker({
-        position: planetarium,
-        map: map,
-        title: "Planetarium",
-        icon: '../images/dolma.png',
-    });
+    // // Try HTML5 geolocation.
+    // if (navigator.geolocation) {
+    //     navigator.geolocation.getCurrentPosition(function (position) {
+    //         let pos = {
+    //             lat: position.coords.latitude,
+    //             lng: position.coords.longitude
+    //         };
 
-    marker.addListener('click', function () {
-        infowindow.open(map, marker);
-    });
-
+    //         infoWindow.setPosition(pos);
+    //         infoWindow.setContent('Location found.');
+    //         infoWindow.open(map);
+    //         map.setCenter(pos);
+    //     }, function () {
+    //         handleLocationError(true, infoWindow, map.getCenter());
+    //     });
+    // } else {
+    //     // Browser doesn't support Geolocation
+    //     handleLocationError(false, infoWindow, map.getCenter());
+    // }
 }
 
-$('.ui.sidebar').sidebar({
-    context: $('.bottom.segment')
-}).sidebar('attach events', '.menu .item');
+function geolocate() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            let geolocation = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            let circle = new google.maps.Circle({
+                center: geolocation,
+                radius: position.coords.accuracy
+            });
+            autocomplete.setBounds(circle.getBounds());
+        });
+    }
+}
+
+
+// Adds a marker to the map.
+function addMarker(location, map) {
+    // Add the marker at the clicked location, and add the next-available label
+    // from the array of alphabetical characters.
+    let marker = new google.maps.Marker({
+        position: location,
+        label: labels[labelIndex++ % labels.length],
+        map: map
+    });
+}
+
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+}
+
+$(document).ready(function () {
+    $('#fullpage').fullpage({
+        autoScrolling: true,
+        scrollHorizontally: true
+    });
+
+    $('.ui.form .ui.selection.dropdown').dropdown({
+        clearable: true
+    });
+
+    $('.clearable.example .ui.inline.dropdown').dropdown({
+        clearable: true,
+        placeholder: 'any'
+    });
+
+
+    $('select.dropdown').dropdown();
+});
