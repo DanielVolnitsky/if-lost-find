2f856e7775beb625bcd654753a538979d8390225
psidae <akson.rill@gmail.com>
2019-02-21 11:37:07 +0100

    Remake

--- project/src/main/resources/static/js/map.js
+++ project/src/main/resources/static/js/map.js
@@ -23,27 +23,6 @@ function initMap() {
     google.maps.event.addListener(map, 'click', function (event) {
         addMarker(event.latLng, map);
     });
-
-
-    // // Try HTML5 geolocation.
-    // if (navigator.geolocation) {
-    //     navigator.geolocation.getCurrentPosition(function (position) {
-    //         let pos = {
-    //             lat: position.coords.latitude,
-    //             lng: position.coords.longitude
-    //         };
-
-    //         infoWindow.setPosition(pos);
-    //         infoWindow.setContent('Location found.');
-    //         infoWindow.open(map);
-    //         map.setCenter(pos);
-    //     }, function () {
-    //         handleLocationError(true, infoWindow, map.getCenter());
-    //     });
-    // } else {
-    //     // Browser doesn't support Geolocation
-    //     handleLocationError(false, infoWindow, map.getCenter());
-    // }
 }
 
 function geolocate() {
