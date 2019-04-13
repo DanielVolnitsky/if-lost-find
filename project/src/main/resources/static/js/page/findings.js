41cef45869d116e97c52559c2135a02faa3876c8
Daniel Volnitsky <waytodanny@gmail.com>
2019-04-13 22:50:46 +0300

    ad ui radius filter block to findings

--- project/src/main/resources/static/js/page/findings.js
+++ project/src/main/resources/static/js/page/findings.js
@@ -1,9 +1,38 @@
+$(document).ready(function() {
+    $('#range').range({
+        min: 0,
+        max: 100,
+        start: 0,
+        onChange: function(value) {
+            $('#display').html(value);
+        }
+    });
+
+    $('#range').addClass('disabled');
+    $('#radius-val-wrapper').hide();
+    $('#radius-btn').addClass('disabled');
+
+
+    $('#radius-cbx').checkbox({
+        onChecked: function() {
+            $('#range').removeClass('disabled');
+            $('#radius-val-wrapper').show();
+            $('#radius-btn').removeClass('disabled');
+        },
+        onUnchecked: function() {
+            $('#range').addClass('disabled');
+            $('#radius-val-wrapper').hide();
+            $('#radius-btn').addClass('disabled');
+        }
+    });
+});
+
 $('.ui.menu .ui.dropdown').dropdown({
     on: 'hover'
 });
-$('.ui.menu a.item').on('click', function() {
+$('.ui.menu a.item').on('click', function () {
     $(this)
         .addClass('active')
         .siblings()
         .removeClass('active');
-})
+});
