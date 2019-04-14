a6dd2ed7c167f735e33d3c7f25083e9d8120b86a
Daniel Volnitsky <waytodanny@gmail.com>
2019-04-14 16:44:37 +0300

    enhanced index page

---
+++ project/src/main/resources/static/js/util.js
@@ -0,0 +1,6 @@
+let semanticUiColors = ["red", "green", "blue", "yellow", "olive", "violet", "orange", "black"
+    , "teal", "purple", "pink", "brown", "grey"];
+
+function getRandomSemanticUiColor() {
+    return semanticUiColors[Math.floor(Math.random() * semanticUiColors.length)]
+}
